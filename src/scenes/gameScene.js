d45a845ddf7e593c2764fea80226b684bd2b68fe
EstebanP <epinczinger@gmail.com>
2021-02-15 23:44:20 -0300

    player stands over moved ground

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -9,30 +9,25 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {}
   create() {
-    const player = this.playerSetup();
     this.background = this.add.tileSprite(400, 300, game.width / 2, 50, "background");
-
     this.ground = this.add
       .tileSprite(400, 550, game.width / 2, 50, "ground")
       .setScale(1.75);
+    this.ground = this.physics.add.existing(this.ground, true);
 
-    let ground;
-    let police;
+    this.player = this.playerSetup();
+    
+    this.police = this.physics.add.image(200, 400, "police").setScale(1.5);
 
-    // this.ground = this.physics.add.staticGroup();
+    this.police.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, this.ground);
+    this.physics.add.collider(this.police, this.ground);
 
+    // this.add.text(50, 50, "Have a good ride", { fill: "#0f0" });
+    // this.add.image(300, 500, "beer").setScale(1);
+    // this.add.image(200, 200, "rock");
 
-    police = this.physics.add.image(200, 400, "police").setScale(1.5);
-
-    police.setCollideWorldBounds(true);
-
-    this.physics.add.collider(player, this.ground);
-    this.physics.add.collider(police, this.ground);
-
-    this.add.text(50, 50, "Have a good ride", { fill: "#0f0" });
-    this.add.image(300, 500, "beer").setScale(1);
-    this.add.image(200, 200, "rock");
-  }
+ }
 
   moveBackground() {
     this.background.tilePositionX += 0.5;
@@ -40,7 +35,7 @@ export default class GameScene extends Phaser.Scene {
   }
 
   playerSetup() {
-    const player = this.physics.add.sprite(300, 100, "skategirl");
+    const player = this.physics.add.sprite(300, 100, "skategirl").setScale(1.5);
 
     player.setBounce(0.1);
     player.setCollideWorldBounds(true);
@@ -49,7 +44,7 @@ export default class GameScene extends Phaser.Scene {
   }
 
   update() {
-    
+
     this.moveBackground();
     // if (this.gameOver) {
     //   return;
