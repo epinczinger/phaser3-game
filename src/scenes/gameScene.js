c03285fecd3b734ae54d490c22573505ff3bd15a
EstebanP <epinczinger@gmail.com>
2021-02-15 23:22:07 -0300

    add movebackgound and player setup

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,46 +1,58 @@
 import "phaser";
 
 export default class GameScene extends Phaser.Scene {
-  
   constructor() {
     super("Game");
+    // this.player = undefined;
+    this.gameOver = false;
   }
 
   preload() {}
   create() {
-   
-    this.add.image(400, 300, "background");
-    let platforms;
-    let player;
+    const player = this.playerSetup();
+    this.background = this.add.tileSprite(400, 300, game.width / 2, 50, "background");
+
+    this.ground = this.add
+      .tileSprite(400, 550, game.width / 2, 50, "ground")
+      .setScale(1.75);
+
+    let ground;
     let police;
 
-    platforms = this.physics.add.staticGroup();
+    // this.ground = this.physics.add.staticGroup();
 
-    platforms.create(400, 550, "ground").setScale(1.8);
-    
 
-    player = this.physics.add.image(300, 100, "skategirl").setScale(0.1);
-    police = this.physics.add.image(200, 400, 'police').setScale(1.5); 
+    police = this.physics.add.image(200, 400, "police").setScale(1.5);
 
-    player.setBounce(0.1);
-    player.setCollideWorldBounds(true);
     police.setCollideWorldBounds(true);
 
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(police, platforms);
+    this.physics.add.collider(player, this.ground);
+    this.physics.add.collider(police, this.ground);
 
-
-    
     this.add.text(50, 50, "Have a good ride", { fill: "#0f0" });
     this.add.image(300, 500, "beer").setScale(1);
     this.add.image(200, 200, "rock");
   }
 
-    
-  update () {
-    
+  moveBackground() {
+    this.background.tilePositionX += 0.5;
+    this.ground.tilePositionX += 1.5;
   }
-  
+
+  playerSetup() {
+    const player = this.physics.add.sprite(300, 100, "skategirl");
+
+    player.setBounce(0.1);
+    player.setCollideWorldBounds(true);
+
+    return player;
+  }
+
+  update() {
+    
+    this.moveBackground();
+    // if (this.gameOver) {
+    //   return;
+    // }
+  }
 }
-
-
--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -15,7 +15,6 @@ export default class PreloaderScene extends Phaser.Scene {
 			{ frameWidth: 40, frameHeight: 56 });
     this.load.image("background", "assets/background.png");
     this.load.image("ground", "assets/ground.png");
-    this.load.image("asphalt", "assets/asphalts.png");
     this.load.image("beer", "assets/beer.png");
     this.load.image("police", "assets/police.png");
     this.load.image("rock", "assets/rock-obstacle.png");
@@ -24,7 +23,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image("phaserLogo", "assets/skater.png");
     this.load.image("box", "assets/ui/grey_box.png");
     this.load.image("checkedBox", "assets/ui/blue_boxCheckmark.png");
-    this.load.audio("bgMusic", ["assets/TownTheme.mp3"]);
+    // this.load.audio("bgMusic", ["assets/TownTheme.mp3"]);
   }
 
   create() {
