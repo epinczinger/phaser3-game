c3c4d0437538bf8e9f9a290a99954cf0a2f57e6d
EstebanP <epinczinger@gmail.com>
2021-02-24 12:41:38 -0300

    add test to scoring API

--- src/helpers.js
+++ src/helpers.js
@@ -11,12 +11,10 @@ export default {
     localStorage.setItem('username', username);
   },
 
-  fetchScores: () => {
-    return fetch(
-      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
-      { mode: 'cors' },
-    ).then((response) => response.json());
-  },
+  fetchScores: () => fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+    { mode: 'cors' },
+  ).then((response) => response.json()),
 
   submitScore: (username, score) => {
     fetch(
