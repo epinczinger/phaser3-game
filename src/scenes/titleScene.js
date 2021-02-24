91e92fb033cd3f39b46bf4ab099a9586821c7abc
EstebanP <epinczinger@gmail.com>
2021-02-23 21:53:54 -0300

    uncomment music

--- src/scenes/titleScene.js
+++ src/scenes/titleScene.js
@@ -41,12 +41,12 @@ export default class TitleScene extends Phaser.Scene {
       'Credits',
     );
 
-    // this.model = this.sys.game.globals.model;
-    // if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-    //   this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
-    //   this.bgMusic.play();
-    //   this.model.bgMusicPlaying = true;
-    //   this.sys.game.globals.bgMusic = this.bgMusic;
-    // }
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
   }
 }
